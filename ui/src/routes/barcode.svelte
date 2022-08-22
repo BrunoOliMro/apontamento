7d2edd45266dd0599199e7985dc0bb1197dd5790
CesarLukaski <juniorlucaski@gmail.com>
2022-08-22 09:58:13 -0300

    dia 22/08

--- ui/src/routes/barcode.svelte
+++ ui/src/routes/barcode.svelte
@@ -4,35 +4,46 @@
   import Title from "../components/title/title.svelte";
 
   let value = "";
-  let codigoBarras;
+  let codigoBarras = "";
   let result = {};
 
   async function doPost() {
     codigoBarras = preSanitize(codigoBarras);
-
     if (codigoBarras.length < 16) {
-      alert("Valor inválido.");
+      popUp();
       codigoBarras = "";
     } else {
-      
-
+      popUpCracha();
       const res = await fetch(`/api/v1/apontamento`, {
         method: "POST",
         body: JSON.stringify({
           codigoBarras: !codigoBarras ? "" : codigoBarras,
         }),
-        headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-      })
-        .then((res) => res.json())
-        .then(console.log);
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }).then((res) => res.json());
     }
   }
 
-  function handleSubmit() {
+  function popUp() {
+    const closePop = (document.getElementById("popUp").style.display = "none");
+  }
+
+  function popUpCracha() {
+    const cracha = (document.getElementById("popUpCracha").style.display =
+      "none");
+  }
+
+  function handleSubmit(event) {
     result = doPost();
+  }
+
+  function getSubmit(event) {
+    if (event.key === "Enter") {
+      handleSubmit();
+    }
   }
 
   function blockForbiddenChars(event) {
@@ -48,13 +59,15 @@
       .join("");
     return sanitizedOutput;
   }
-  
+
+  //local Storage Front
   function setValues() {
     var values = value;
     localStorage.setItem("barcodeData", values);
   }
 </script>
 
+<svelte:window on:keyup={getSubmit} />
 <main>
   <div>
     <Breadcrumb />
@@ -62,14 +75,25 @@
 
     <div class="bar">Código de barras</div>
     <!-- <form on:submit|preventDefault={handleSubmit} > -->
-    <form action="/api/v1/apontamento" method="POST">
+    <form action="/api/v1/apontamento" method="POST" on:submit={handleSubmit}>
       <label class="input">
+        <div class="modal_body">
+          <div id="popUp">
+            <div id="button" on:click={popUp}>X</div>
+            <div>Codigo de barras Invalido</div>
+          </div>
+
+          <div id="popUpCracha">
+            <div id="button" on:click={popUpCracha}>X</div>
+            <div>Selecione quem irá produzir essa peça</div>
+            <input id="inputText" type="text" />
+          </div>
+        </div>
+
         <input
           bind:value
           on:input={blockForbiddenChars}
           on:input={setValues}
-          on:keyup={handleSubmit}
-
           id="codigoBarras"
           name="codigoBarras"
           minlength="16"
@@ -97,5 +121,37 @@
     align-items: center;
     text-align: center;
     display: flex;
+  }
+
+  #inputText {
+    background-color: black;
+    color: white;
+  }
+
+  #button {
+    display: flex;
+    justify-content: right;
+    align-items: center;
+    text-align: center;
+  }
+
+  #popUpCracha {
+    padding: 15px;
+    font-size: 35px;
+    border-radius: 5px;
+    display: block;
+    position: absolute;
+    background-color: black;
+    color: white;
+  }
+
+  #popUp {
+    padding: 20px;
+    font-size: 35px;
+    border-radius: 5px;
+    display: block;
+    position: absolute;
+    background-color: black;
+    color: white;
   }
 </style>
